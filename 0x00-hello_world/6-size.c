c8337d51cf1f65f1832dad19b21e2f8459015a6d
Kolapo-code <kolapoadedipe36@gmail.com>
2022-12-08 07:44:02 -0800

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,18 +5,11 @@
 */
 
 int main(void)
-    token
-    8 | }
-      |
-    9 | printf("Size of a char: %zu byte(s)\n", sizeof(char));
-      |
-   10 | printf("Size of an int: %zu byte(s)\n", sizeof(int));
-      |
-   11 | printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-      |
-   12 | printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-      |
-   13 | printf("Size of a float: %zu byte(s)\n", sizeof(float));
-      |
-   14 | return (0);
-      | }
+}
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
