75bd2959e07c81d0b4d665ad6adc9a2356ad2fa8
Kolapo-code <kolapoadedipe36@gmail.com>
2022-12-09 11:44:08 -0800

    Task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -8,7 +7,7 @@ int main(void)
 {
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
-	    putchar(c);
-        putchar('\n');
+		putchar(c);
+	putchar('\n');
 	return (0);
 }
