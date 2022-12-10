db990328e6332012fad5c7f5d78cf9fe3c09dc85
Kolapo-code <kolapoadedipe36@gmail.com>
2022-12-10 00:46:10 -0800

    Task5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,7 @@
  */
 int main(void)
 {
-	int num;
-
-	for (num = 0; num < 10; num++)
-		printf("%d", &num);
-	printf('\n');
+	for (int num = 0; num < 10; num++)
+		printf("%d\n", num);
 	return (0);
 }
