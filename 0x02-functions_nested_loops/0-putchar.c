c34cd57b0c25202af59830735fa99809574f165b
Kolapo-code <kolapoadedipe36@gmail.com>
2022-12-14 07:52:05 -0800

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,6 @@
 int main(void)
 
 {
-
 	   _putchar('_');
 	   _putchar('p');
 	   _putchar('u');
@@ -20,6 +19,5 @@ int main(void)
 	   _putchar('r');
 	   _putchar('\n');
 	   
-	   return (0);
-	   
+	   return (0)
 }
