fb158128db36cd9dbe3eada16e83b3c84e6aac5b
Kolapo-code <kolapoadedipe36@gmail.com>
2022-12-14 08:58:53 -0800

    Task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,20 +5,20 @@
  * 
  * Return: Always 0 (Success)
  */
+void print_alphabet(void)
+{
+	char letter;
+
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+
+	_putchar('\n');
+}
 
 int main(void)
 {
-	void print_alphabet(void)
-	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
-	}
 	print_alphabet();	
 	return (0);
 }
