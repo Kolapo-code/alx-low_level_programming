58b9df64e4325a1f558bca5c0c78e4d8db367fae
Kolapo-code <kolapoadedipe36@gmail.com>
2023-01-12 02:25:49 -0800

    Task0-101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,27 +1,15 @@
 #include "main.h"
-
 #include <stdlib.h>
-
 #include <stdio.h>
-
-
 
 #define ERR_MSG "Error"
 
-
-
 /**
-
  * is_digit - checks if a string contains a non-digit char
-
  * @s: string to be evaluated
-
  *
-
  * Return: 0 if a non-digit is found, 1 otherwise
-
 */
-
 int is_digit(char *s)
 
 {
@@ -46,20 +34,12 @@ int is_digit(char *s)
 
 }
 
-
-
 /**
-
  * _strlen - returns the length of a string
-
  * @s: string to evaluate
-
  *
-
  * Return: the length of the string
-
 */
-
 int _strlen(char *s)
 
 {
@@ -80,14 +60,9 @@ int _strlen(char *s)
 
 }
 
-
-
 /**
-
  * errors - handles errors for main
-
 */
-
 void errors(void)
 
 {
@@ -98,22 +73,13 @@ void errors(void)
 
 }
 
-
-
 /**
-
  * main - multiplies two positive numbers
-
  * @argc: number of arguments
-
  * @argv: array of arguments
-
  *
-
  * Return: always 0 (Success)
-
 */
-
 int main(int argc, char *argv[])
 
 {
